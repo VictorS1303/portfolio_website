30a7d89e6573a71a9df50df60801edce06f2e4bd
VictorS1303 <vict703k@stud.kea.dk>
2023-12-22 13:56:35 +0100

    Correct content inserted on all pages

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const mainNav = document.getElementById('main_nav')
 
 const topLayer = document.getElementById('top_layer')
 const animationHeaderSpans = document.querySelectorAll('.animation-header > span')
+const webHeaderSpans = document.querySelectorAll('.web-header > span')
+const contentHeaderSpans = document.querySelectorAll('.content-header > span')
+const animationHeaderVideo = document.getElementById('animation_header_video')
 
-
-console.log(animationHeaderSpans)
 
 // EVENT LISTENERS //
 mobileMenuToggleBtn.addEventListener('click', toggleMobileMenu)
 topLayer.addEventListener('click', hideTopLayer)
-// topLayer.addEventListener('pointerout', revealTopLayer)
 
 // FUNCTIONS //
 
@@ -30,23 +30,52 @@ function updateMobileMenuToggleIcon()
         : (mobileMenuToggleBtn.classList.add('fa-bars'), mobileMenuToggleBtn.classList.remove('fa-times'))
 }
 
+// Hide Top Layer
 function hideTopLayer()
 {
+    // Add hidden class to top layer
     topLayer.classList.add('hidden')
 
+    // Check if top layer contains the class of 'hidden'
     if (topLayer.classList.contains('hidden'))
     {
-        animationHeaderSpans.forEach((animationHeaderSpan) =>
-        {
-            setTimeout(() =>
-            {
-                animationHeaderSpan.style.animationPlayState = 'running'
-            }, 400)
-        })
+        // Call the runHeaderAnimation function
+        runHeaderAnimation()
+
+        // Play Background Video
+        animationHeaderVideo.play()
     }
 }
 
-// function revealTopLayer()
-// {
-//     topLayer.classList.remove('hidden')
-// }
+// Run Header Animation
+function runHeaderAnimation()
+{
+    // Looping through each span inside the header
+    animationHeaderSpans.forEach((animationHeaderSpan) =>
+    {
+        // Set the play state of each span 'running' 500ms after the top layer has been hidden
+        setTimeout(() =>
+        {
+            animationHeaderSpan.style.animationPlayState = 'running'
+        }, 500)
+    })
+
+
+    webHeaderSpans.forEach((webHeaderSpan) =>
+    {
+        // Set the play state of each span 'running' 500ms after the top layer has been hidden
+        setTimeout(() =>
+        {
+            webHeaderSpan.style.animationPlayState = 'running'
+        }, 500)
+    })
+
+    contentHeaderSpans.forEach((contentHeaderSpan) =>
+    {
+        // Set the play state of each span 'running' 500ms after the top layer has been hidden
+        setTimeout(() =>
+        {
+            contentHeaderSpan.style.animationPlayState = 'running'
+        }, 500)
+    })
+}
